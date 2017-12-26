39ca853cb3ce9e2dc1aee7fd966fcb3d53955858
Mockingbird321 <navruzov.e1511@gmail.com>
2017-12-26 22:33:46 +0300

    Add files via upload

---
+++ Домашнее задание/Program.fs
@@ -0,0 +1,34 @@
+﻿#light
+open System
+
+let rec read() =
+    match System.Double.TryParse(System.Console.ReadLine()) with
+    | false, _ -> printfn "Вы ввели неправильные данные. Попробуйте заново!"; read()
+    | _, x -> x
+
+printfn "А:"
+let a = read();
+printfn "В:"
+let b = read();
+printfn "С:"
+let c = read();
+
+type ResultOfSolve=
+ None
+    |Function of float*float
+
+let solve = fun (a,b,c) ->
+    let D = b*b-4.0*a*c
+    if a=0.0 then
+        if b=0.0 then None 
+        else Function((-c/b),(-c/b))
+    else
+        if D<0.0 then None 
+        else Function(((-b+sqrt(D))/(2.0*a),(-b-sqrt(D))/(2.0*a)))
+
+let res = solve(a,b,c);
+
+match res with
+    None -> printf "Нет решений"
+    |Function(x1,x2) -> printf "Уравнение имеет корни %f %f " x1 x2
+System.Console.Read();
